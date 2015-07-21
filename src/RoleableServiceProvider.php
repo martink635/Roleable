224c32f8fec02163a81eba2a38e463e68996251c
Martin Koterle <martin.koterle@gmail.com>
2015-07-21 02:14:15 +0200

    Generate migration date prefix

--- src/RoleableServiceProvider.php
+++ src/RoleableServiceProvider.php
@@ -24,7 +24,20 @@ class RoleableServiceProvider extends ServiceProvider
     {
         $this->publishes([
             __DIR__.'/../config/config.php' => config_path('roleable.php'),
-            __DIR__.'/../database/migrations/' => database_path('migrations')
+            __DIR__.'/../database/migrations/create_roles_table.php' => database_path("migrations/{$this->getDatePrefix('-15 seconds')}_create_roles_table.php"),
+            __DIR__.'/../database/migrations/create_permissions_table.php' => database_path("migrations/{$this->getDatePrefix('-10 seconds')}_create_permissions_table.php"),
+            __DIR__.'/../database/migrations/create_role_user_table.php' => database_path("migrations/{$this->getDatePrefix('-5 seconds')}_create_role_user_table.php"),
+            __DIR__.'/../database/migrations/create_permission_role_table.php' => database_path("migrations/{$this->getDatePrefix()}_create_permission_role_table.php")
         ]);
+    }
+
+    /**
+     * Get the date prefix for the migration.
+     *
+     * @return string
+     */
+    private function getDatePrefix($time = "now")
+    {
+        return date('Y_m_d_His', strtotime($time));
     }
 }
